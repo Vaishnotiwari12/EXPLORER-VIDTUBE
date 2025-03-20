5f8c98158bb972a803d192819f33b489a8f32e0b
Vaishnotiwari12 <vaishnotiwari12@gmail.com>
2025-03-20 09:30:55 +0530

    asyncHandler

---
+++ src/utils/ApiError.js
@@ -0,0 +1,25 @@
+class ApiError extends Error {
+    Constructor(
+        stausCode,
+        message = "Something went wrong",
+        error = [],
+        stack = ""     
+
+    ){
+        super(message)
+        this.stausCode = stausCode
+        this.data = data
+        this.message = message
+        this.success = false
+        this.errors = errors;
+
+        if(stack){
+            this.stack = stack
+        }else {
+            Error.captureStackTrace(this.this.Constructor)
+        }
+
+    }
+}
+
+export {ApiError}
