6b30033ce90b634c3ebfaadf911750387da6fe21
Vaishnotiwari12 <vaishnotiwari12@gmail.com>
2025-03-21 23:44:16 +0530

    middleware

--- src/app.js
+++ src/app.js
@@ -1,5 +1,8 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
+import jwt from 'jsonwebtoken';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
@@ -14,6 +17,35 @@ app.use(
 app.use(express.json({limit:'16mb'}));
 app.use(express.urlencoded({extended:true,limit:'16mb'}));
 app.use(express.static('public'))
+
+const secret = process.env.ACCESS_TOKEN_SECRET;
+if (!secret) {
+    console.error("Error: ACCESS_TOKEN_SECRET is undefined.");
+    process.exit(1); // Stop the app if it's missing
+}
+
+
+const token = jwt.sign(
+  { foo: 'bar' },
+  process.env.ACCESS_TOKEN_SECRET,
+  { expiresIn: '1h' }
+);
+
+console.log("Generated Token:", token);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 export {app}
---
+++ src/controllers/healthcheck.controller.js
@@ -0,0 +1,14 @@
+import { upload } from "../middlewares/multer.middlewares.js";
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+import {Router} from 'express';
+
+const healthcheck = asyncHandler(async (req, res) => {
+    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+})
+
+const router = Router();
+router.route('/').get(healthcheck);
+
+export default router
