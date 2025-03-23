01ba62504767b3f478ff51b64cf3847ae71498ec
Vaishnotiwari12 <vaishnotiwari12@gmail.com>
2025-03-23 10:39:33 +0530

    healthcheck.controller.js

--- src/app.js
+++ src/app.js
@@ -15,5 +15,11 @@ app.use(express.json({limit:'16mb'}));
 app.use(express.urlencoded({extended:true,limit:'16mb'}));
 app.use(express.static('public'))
 
+//import routes 
+import healthcheckRouter from './routes/healthcheck.routes.js';
+
+//use routes
+app.use("/api/v1/healthcheck",healthcheckRouter);
+
 
 export {app}
